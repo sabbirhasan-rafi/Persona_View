5b1ad93e0959d3cb333b4639768002114ffb69d3
Rafi Hasan <mdsabbirhasanrafi6@gmail.com>
2026-04-08 18:02:07 +0600

    create navbar component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Section from "./components/Section";
+
+const App = () => {
+  return (
+    <div className="app_layout">
+      <Navbar />
+      <Section />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-content">
+        <h1 className="navbar-title-badge">TARGET AUDIENCE</h1>
+        <p className="navbar-platform-badge">DIGITAL BANKING PLATFORM</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
