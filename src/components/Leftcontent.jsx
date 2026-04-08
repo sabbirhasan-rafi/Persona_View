a6f07de451884619869aa91201b8ce13619a1112
Rafi Hasan <mdsabbirhasanrafi6@gmail.com>
2026-04-08 21:25:07 +0600

    Left_Content_Component

---
+++ src/components/Leftcontent.jsx
@@ -0,0 +1,32 @@
+import { MdOutlineArrowOutward } from "react-icons/md";
+const Leftcontent = () => {
+  return (
+    <div className="left-content">
+      <div className="top">
+        <h1
+          className="
+      left_content_heading"
+        >
+          Prospective
+          <br />
+          customer
+          <br />
+          segmentation
+        </h1>
+
+        <p className="para">
+          Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit.
+          Repellendus natus in quisquam <br /> placeat modi voluptatibus illo ea
+          qui numquam delectus! <br /> Aperiam, at culpa.
+        </p>
+      </div>
+      <div className="bottom">
+        <h2 className="arrowIcon">
+          <MdOutlineArrowOutward />
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Leftcontent;
---
+++ src/components/Section.jsx
@@ -0,0 +1,12 @@
+import Leftcontent from "./Leftcontent";
+import Rightcards from "./Rightcards";
+
+const Section = () => {
+  return (
+    <div className="section">
+      <Leftcontent />
+    </div>
+  );
+};
+
+export default Section;
